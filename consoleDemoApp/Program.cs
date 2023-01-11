77c86d50387111e9020fc131dd6685698ae61674
Shubham Kumawat <Shubham Kumawat@DESKTOP-7BH01OF>
2023-01-11 12:56:02 +0530

    Add project files.

---
+++ consoleDemoApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ClassDemo;
+namespace consoleDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo DemoObject = new Demo();
+            DemoObject.printFunt();
+        }
+    }
+}
